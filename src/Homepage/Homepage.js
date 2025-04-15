3333832bc7d36f4753a77cf85fe4d4d16418b382
Francesco Barone <francescobarone@Mac.homenet.telecomitalia.it>
2025-04-15 13:14:30 +0200

    logo

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -7,7 +7,8 @@ import { faLinkedin, faFacebookF, faWhatsapp } from '@fortawesome/free-brands-sv
 import teamMember1 from '../Images/prova1.png';
 import teamMember2 from '../Images/prova2.png';
 //import logo from '../Images/sm-reverse.png';
-import logo from '../Images/logo.png';
+import logo from '../Images/LogoMesteso.png';
+import logo2 from '../Images/LogoM1.png';
 
 const Homepage = () => {
   const chatInputRef = useRef(null);
@@ -417,7 +418,7 @@ const Homepage = () => {
       <footer className="footer" id="contatti-container">
         <div className="footer-container">
           <div className="footer-section">
-            <img src={logo} alt="Studio Malacarne" className="footer-logo" />
+            <img src={logo2} alt="Studio Malacarne" className="footer-logo" />
             <form className="footer-contact-form">
               <input 
                 type="text" 
